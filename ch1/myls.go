844c044dacf6cecb427d0b4cf337a4c91ae93435
Ilan Pillemer <ilan.pillemer@gmail.com>
2018-08-06 23:44:04 +0100

    delete irritating comment noise

--- ch1/myls.go
+++ ch1/myls.go
@@ -29,24 +29,10 @@ func main() {
 	}
 
 	buff := make([]byte, 256) // magic number. but needs to big enough to prevent infinite loops into buffer.
-
-	// drain all directory entries buffer of directory entries...
-	// each call to ReadDirent updates the buffer and returns the
-	// number of bytes read or zero when at the end of the
-	// directory.
-
 	for n, _ := unix.ReadDirent(fd, buff); n != 0; n, _ = unix.ReadDirent(fd, buff) {
 		var p []byte
-		// make sure nothing is lost as there may be some end stuff needed...
 		p = append(p, buff...)
-		// drain the data in p until only a little is left that is not enough...
 		for len(p) > 0 {
-			// get first directory entry record in the buffer
-
-			// get actual length of record, requires
-			// reading it from the c struct, which we know
-			// we are at the beginning of
-
 			rl_offset := unsafe.Offsetof(Dirent{}.Reclen)
 			//rl_size := unsafe.Sizeof(Dirent{}.Reclen) // will always be 2, as its a uint16
 			if int(rl_offset) > len(p) || int(rl_offset+1) > len(p) {
@@ -56,11 +42,9 @@ func main() {
 			if int(rl) > len(p) {
 				break
 			}
-			// put record into its own slice
 			record := p[:rl]
 			//nl_size := unsafe.Sizeof(Dirent{}.Namlen)
 			if len(record) == 0 {
-				//n, _ = unix.ReadDirent(fd, p)
 				break
 			} else {
 				nl_offset := unsafe.Offsetof(Dirent{}.Namlen)
@@ -74,7 +58,6 @@ func main() {
 				name := record[name_offset : name_offset+nl]
 				fmt.Println(string(name))
 
-				// drain buffer
 				p = p[rl:]
 			}
 		}
