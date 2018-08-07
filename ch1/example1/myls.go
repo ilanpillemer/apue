fdcfe274ee4581bfb3456c77d54271daee7097b5
Ilan Pillemer <ilan.pillemer@gmail.com>
2018-08-07 18:54:03 +0100

    move defer closer

--- ch1/example1/myls.go
+++ ch1/example1/myls.go
@@ -12,11 +12,12 @@ import (
 
 func main() {
 	dir, err := C.opendir(C.CString(os.Args[1]))
+	defer C.closedir(dir)
+
 	if err != nil {
 		fmt.Printf("can't open %s: %v\n", os.Args[1], err)
 		return
 	}
-	defer C.closedir(dir)
 
 	for dirent := C.readdir(dir); dirent != nil; dirent = C.readdir(dir) {
 		name := C.GoString(&dirent.d_name[0])
