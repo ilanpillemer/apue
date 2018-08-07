78b47c537c19d110870261b457383ce239ab02ab
Ilan Pillemer <ilan.pillemer@gmail.com>
2018-08-07 22:48:53 +0100

    first stab, need to still check if loops are idiomatic

---
+++ ch1/example2/mycp.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"unsafe"
+)
+
+/*
+#include <unistd.h>
+*/
+import "C"
+
+const bufferSize = 4096
+
+func main() {
+
+	var buf [bufferSize]byte
+	n, _ := C.read(C.STDIN_FILENO, unsafe.Pointer(&buf[0]), C.ulong(bufferSize))
+	for n != 0 {
+		if w := C.write(C.STDOUT_FILENO, unsafe.Pointer(&buf[0]), C.ulong(n)); w != n {
+			fmt.Println("write error")
+			os.Exit(1)
+		}
+		n = C.read(C.STDIN_FILENO, unsafe.Pointer(&buf[0]), C.ulong(bufferSize))
+		if n < 0 {
+			fmt.Println("read error")
+			os.Exit(1)
+		}
+	}
+}
