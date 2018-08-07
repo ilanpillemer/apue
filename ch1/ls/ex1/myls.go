51ff1707be5b6ff0020aa15e969ea1cd19713f4b
Ilan Pillemer <ilan.pillemer@gmail.com>
2018-08-07 15:24:51 +0100

    Wooot! Eventually... wow, that was difficult to understand...

---
+++ ch1/ls/ex1/myls.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+#include <dirent.h>
+*/
+import "C"
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	dir, err := C.opendir(C.CString(os.Args[1]))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer C.closedir(dir)
+
+	for dirent := C.readdir(dir); dirent != nil; dirent = C.readdir(dir) {
+		name := C.GoString(&dirent.d_name[0])
+		fmt.Println(name)
+	}
+
+}
