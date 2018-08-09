e9afd8cfcea955e9512bbae3b9cdb4d057cd0f65
Ilan Pillemer <ilan.pillemer@gmail.com>
2018-08-10 01:59:39 +0200

    remove incorrect negative

--- ch1/example3/mycp.go
+++ ch1/example3/mycp.go
@@ -15,7 +15,7 @@ func main() {
 			log.Fatalf("input error: %v\n", inErr)
 		}
 		// this does not work when being redirected..
-		// but does not work when run normally, but does not write to screen though..
+		// but does work when run normally, but does not write to screen though..
 		wtf, err := C.putc(char, C.stdout)
 		fmt.Printf("%c", wtf) // this does work when redirected....
 		if err != nil {
