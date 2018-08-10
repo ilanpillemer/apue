32e831c348b2e286e38854b1261d16622fd046d7
Ilan Pillemer <ilan.pillemer@gmail.com>
2018-08-10 02:09:51 +0200

    flush it!

--- ch1/example3/mycp.go
+++ ch1/example3/mycp.go
@@ -14,12 +14,14 @@ func main() {
 		if inErr != nil {
 			log.Fatalf("input error: %v\n", inErr)
 		}
-		// this does not work when being redirected..
-		// but does work when run normally, but does not write to screen though..
 		wtf, err := C.putc(char, C.stdout)
 		fmt.Printf("%c", wtf) // this does work when redirected....
 		if err != nil {
 			log.Fatalf("output error: %v\n", err)
 		}
 	}
+
+	// C only flushes when it detects that stdout is a terminal. So
+	// this is needed when piping..
+	C.fflush(C.stdout)
 }
